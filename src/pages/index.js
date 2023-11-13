9483eea757ef6e3802878e8ab2eea4b44281c6cd
Michelle Brown <m.andreabr@gmail.com>
2023-11-13 14:21:09 +1100

    import silkscreen for display font

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,19 +1,20 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
-// import { Silkscreen } from 'next/font/google';
+import { Silkscreen } from 'next/font/google';
 import { Button } from 'react-daisyui'
 
 const inter = Inter({ subsets: ['latin'] })
-// const silkscreen = Silkscreen( {
-//   subsets: ['latin'],
-//   display: 'swap',
-//   variable: '--font-silkscreen',
-// });
+const silkscreen = Silkscreen( {
+  subsets: ['latin'],
+  weight: ['400','700'],
+  display: 'swap',
+  variable: '--font-silkscreen',
+});
 
 export default function Home() {
   return (
     <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
+      className={`flex min-h-screen flex-col items-center justify-between p-24 ${silkscreen.className}`}
     >
       <Button color="primary">Button</Button>
     </main>
