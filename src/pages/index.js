1b8fc20451636cd936f9f84eca99a9a747d6b6f1
Michelle Brown <m.andreabr@gmail.com>
2023-11-13 14:17:15 +1100

    install daisy and react daisy

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,21 @@
-// import Image from 'next/image'
-import { Pixelify_Sans } from 'next/font/google'
+import Image from 'next/image'
+import { Inter } from 'next/font/google'
+// import { Silkscreen } from 'next/font/google';
+import { Button } from 'react-daisyui'
 
-const pixelifySans = Pixelify_Sans({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'] })
+// const silkscreen = Silkscreen( {
+//   subsets: ['latin'],
+//   display: 'swap',
+//   variable: '--font-silkscreen',
+// });
 
 export default function Home() {
   return (
-    <main className={pixelifySans.className}>
-      <button className="btn btn-primary">Test</button>
-      <h1>GameList GAMELIST</h1>
+    <main
+      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
+    >
+      <Button color="primary">Button</Button>
     </main>
   )
 }
