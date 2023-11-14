f18f0d66f6c0096aa0fa29f65063d8c7b5bed4e2
Michelle Brown <m.andreabr@gmail.com>
2023-11-14 15:53:43 +1100

    remove daisy footer component to fix spacing issue, underline a tag anywhere except nav

--- src/components/AppFooter.js
+++ src/components/AppFooter.js
@@ -1,11 +1,9 @@
-import { Footer } from "react-daisyui";
-
 export default function AppFooter() {
     return (
-        <Footer className="app-footer p-4 bg-primary text-secondary-content flex place-content-center">
-            <div className="flex justify-center">
+        <footer className="app-footer p-4 bg-primary text-secondary-content text-center">
+            <div>
                 All data provided by <a href="https://rawg.io" rel="noreferrer" target="_blank">RAWG.io</a>
             </div>
-        </Footer>
+        </footer>
     )
 }
