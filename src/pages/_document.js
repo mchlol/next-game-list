2df3e6c7624442ae649658440d856a2a6e4915ce
Michelle Brown <m.andreabr@gmail.com>
2023-11-15 09:25:38 +1100

    add page title

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en" data-theme="retro">
-      <Head />
+      <Head>
+        <title>GameList</title>
+      </Head>
       <body>
         <Main />
         <NextScript />
