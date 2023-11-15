28926c2fa7dc26262f5c548595469fd2c2eeb845
Michelle Brown <m.andreabr@gmail.com>
2023-11-15 13:26:15 +1100

    add description & styles

--- src/pages/[slug].js
+++ src/pages/[slug].js
@@ -1,7 +1,10 @@
 import { useState, useEffect } from 'react';
 import { useRouter } from 'next/router';
+import Link from 'next/link';
 import axios from 'axios';
-import { Loading } from 'react-daisyui';
+import { Loading, Card, Button, Badge } from 'react-daisyui';
+import { formatDate, joinArray, joinPlatformArray } from '@/functions';
+import GameCard from '@/components/GameCard';
 
 export default function ViewGame() {
     const router = useRouter();
@@ -11,11 +14,13 @@ export default function ViewGame() {
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null);
 
-    console.log('slug: ',slug);
-    console.log('gameData: ',gameData);
-
     useEffect( () => {
 
+        loadGameDetails(slug);
+
+    },[slug]);
+
+    function loadGameDetails(slug) {
         setLoading(true);
         
         axios.get(`https://rawg.io/api/games/${slug}?key=${process.env.NEXT_PUBLIC_API_KEY}`)
@@ -29,8 +34,7 @@ export default function ViewGame() {
             setError(err);
             setLoading(false);
         })
-
-    },[slug]);
+    }
 
     if (error) {
         return <p>Could not load game data.</p>
@@ -42,8 +46,66 @@ export default function ViewGame() {
                 loading
                 ? <div> <Loading /> </div>
                 : 
-                <div>
-                    {slug}
+                <div className="m-4">
+
+                    <div className="m-4 flex flex-wrap justify-between">
+                        <h2>{gameData.name}</h2>
+                        <Button type="button"
+                    onClick={ () => router.back()}
+                    >
+                        Back
+                    </Button>
+                    </div>
+
+                    <div className="m-4">
+                        { 
+                            <img 
+                        src={gameData.background_image} 
+                        alt={gameData.name} 
+                        className="view-game-img"
+                        /> 
+                        }
+                    </div>
+
+                    <Card className="m-4 p-4">
+                        <p><strong>Released:</strong> <span >{formatDate(gameData.released)}</span> </p>
+                        <p><strong>Developers:</strong> {joinArray(gameData.developers)}</p>
+                        <p><strong>Platforms:</strong> {joinPlatformArray(gameData.parent_platforms)}</p>
+                        <p><strong>Genres:</strong> {joinArray(gameData.genres)}</p>
+                        <p>
+                        <strong>Metacritic rating:</strong> {gameData.metacritic ? <Badge color="accent">{gameData.metacritic}</Badge> : <span>N/A</span> }
+                        </p>
+                    </Card>
+
+                    <div className="m-4">
+                        <Button className="m-1">
+                            Add to wishlist
+                        </Button>
+                        <Button className="m-1">
+                            Add to favourites
+                        </Button>
+                    </div>
+
+                    <div className="game-description m-4">
+                        <p 
+                        dangerouslySetInnerHTML={
+                            { __html: gameData.description}
+                            }
+                        >
+
+                        </p>
+                    </div>
+
+                    <div className="m-4">
+                        <h3>Screenshots</h3>
+                    </div>
+
+                    <Link className="m-4" href={`https://rawg.io/games/${gameData.slug}/suggestions`} target="_blank" rel="noreferrer">
+                        <Button>
+                            View similar games on RAWG.io
+                        </Button>
+                    </Link>
+
                 </div>
             }
         </div>
