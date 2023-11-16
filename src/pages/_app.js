1b8adcbe5c156e6ec08b41f6edb0b09163e470ae
Michelle Brown <m.andreabr@gmail.com>
2023-11-16 16:10:36 +1100

    move title to head in _app component

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,10 +1,16 @@
 import '@/styles/globals.css'
+import Head from 'next/head'
 import Layout from './layout'
 
 export default function App({ Component, pageProps }) {
   return (
-  <Layout>
-    <Component {...pageProps} />
-  </Layout>
+    <>
+    <Head>
+      <title>GameList</title>
+    </Head>
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  </>
   )
 }
