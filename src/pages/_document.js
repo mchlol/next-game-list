414262ff8fad7583ea4b4353c88837b869e69a15
Michelle Brown <m.andreabr@gmail.com>
2023-11-20 14:23:58 +1100

    set to dark theme

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -2,7 +2,7 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
-    <Html lang="en" data-theme="retro">
+    <Html lang="en" data-theme="synthwave">
       <Head />
       <body>
         <Main />
