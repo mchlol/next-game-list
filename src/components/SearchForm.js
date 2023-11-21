de7463d65e2a134af400668af1c8376f46962684
Michelle Brown <m.andreabr@gmail.com>
2023-11-21 19:30:36 +1100

    add a button to search for popular ames

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -10,25 +10,30 @@ export default function SearchForm() {
   
     function handleSubmit(ev) {
       ev.preventDefault();
-      // go to dynamic route /search/[searchQuery]
-      if (searchQuery === '') {
-        router.push(`/${searchQuery}`)
-      } else {
       router.push(`/search/${searchQuery}`);
-      }
+    }
+
+    function handlePopularClick() {
+      setSearchQuery('popular');
+      router.push(`/search/${searchQuery}`);
     }
 
     return (
         <form className="join" id="searchForm" onSubmit={handleSubmit}>
             <Input bordered 
+            required
             id="search-input"
             type="text" 
             value={searchQuery}
             placeholder="Search for a game title" 
             autoComplete="off"
             className="join-item" 
-            onChange={ev => setSearchQuery(ev.target.value)} /> 
+            onChange={ev => setSearchQuery(ev.target.value)} 
+            /> 
             <Button type="submit" className="btn btn-secondary join-item">Search</Button>
+            <Button type="button" className="btn btn-primary join-item"
+            onClick={handlePopularClick}
+            >Popular</Button>
         </form>
     )
 }
