2dacc7b1fb712e4a695799486f746710b29453fe
Michelle Brown <m.andreabr@gmail.com>
2024-03-16 14:35:29 +1100

    restructure page content and update links

--- src/pages/index.js
+++ src/pages/index.js
@@ -21,9 +21,12 @@ export default function Home() {
 
           <div className="p-4 flex items-center justify-center">
 
-            <h2 className="text-l text-center m-32">For cross-platform gamers</h2>
+            <div>
+              <h2 className="text-l text-center m-32">For cross-platform gamers</h2>
+              <p>Whether you're a PC aficionado, a console connoisseur, or an indie game enthusiast, we've got you covered. Simply search our extensive database spanning all platforms - PC, PlayStation, Xbox, and even indie gems on itch.io - and find the game that suits your style.</p>
+            </div>
 
-            <div class="icon-1 absolute top-8 right-8">
+            <div className="icon-1 absolute top-8 right-8">
               <IoGameControllerOutline 
               className="text-6xl" 
               role="img" aria-hidden="true"
@@ -32,8 +35,8 @@ export default function Home() {
 
           </div>
 
-          <div class="image-blend-screen">
-            <Image src={image01} alt="a collection of retro game `paraphernalia" />
+          <div className="image-blend-screen">
+            <Image className="fade-img" src={image01} alt="a collection of retro game `paraphernalia" />
           </div>
 
       </div>
@@ -42,9 +45,10 @@ export default function Home() {
 
         <div className="p-4 flex items-center justify-center md:order-last relative">
 
-            <h2 className="text-xl text-center m-32">Search games from any platform, including Steam and Itch.</h2>
+            <h2 className="text-xl text-center m-32">never miss out on an epic adventure again!</h2>
+            <p>Save games you're interested in for later, and when you find those games that truly captivate you, add them to your favorites list for quick access whenever the gaming mood strikes.</p>
 
-            <div class="icon-1 absolute top-8 left-8">
+            <div className="icon-1 absolute top-8 left-8">
               <SiGamebanana 
               className="text-6xl" 
               role="img" aria-hidden="true"
@@ -54,7 +58,7 @@ export default function Home() {
           </div>
 
         <div className="image-blend-screen md:order-first">
-          <Image src={image02} alt="an Xbox controller on a backlit mousepad" />
+          <Image className="fade-img" src={image02} alt="an Xbox controller on a backlit mousepad" />
         </div>
         
       </div>
@@ -63,17 +67,23 @@ export default function Home() {
       
         <div className="flex flex-col items-center justify-center m-32">
 
-          <div class="icon-1 absolute top-8 right-8">
+          <div className="icon-1 absolute top-8 right-8">
             <MdGamepad
             className="text-6xl" 
             role="img" aria-hidden="true"
             />
           </div>
 
+          <ul>
+            <li><strong>One-Stop Gaming Hub:</strong> Access thousands of games across multiple platforms in one centralized location.</li>
+            <li><strong>Save for Later:</strong> Create your personalized wishlist and keep track of games you're eager to play.</li>
+            <li><strong>Never Miss a Beat:</strong> Easily save your favorite games for quick access and endless entertainment.</li>
+          </ul>
+
           <h2 className="text-xl text-center">Ready to explore?</h2>
 
           <Button className="mt-4">
-            <Link href="/search">
+            <Link href="/searchgames">
               Search now!
             </Link>
           </Button>
