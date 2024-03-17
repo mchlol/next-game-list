9a5a8087dd3e7c77747e6ca86d3890fa97a6b7cd
Michelle Brown <m.andreabr@gmail.com>
2024-03-17 16:25:21 +1100

    reposition elements with tailwind classes

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,18 +7,18 @@ import image02 from "../assets/jose-gil-2pNdTBn4C7U-unsplash.jpg"
 import Link from "next/link";
 import { IoGameControllerOutline } from "react-icons/io5";
 import { SiGamebanana } from "react-icons/si";
-import { MdGamepad } from "react-icons/md";
+import { SiGamejolt } from "react-icons/si";
+
 import Banner from "@/components/Banner";
 
 export default function Home() {
 
   return (
-    <div className="home md:grid md:grid-flow-row flex flex-wrap flex-col">
+    <div className="home flex flex-col">
 
       <Hero />
 
-      <Banner>
-
+      <Banner imageSrc={image01} altText="a collection of retro game paraphernalia" imagePosition="right">
         <Banner.Icon>
           <IoGameControllerOutline />
         </Banner.Icon>
@@ -30,75 +30,35 @@ export default function Home() {
         <Banner.Text>
           Whether you're a PC aficionado, a console connoisseur, or an indie game enthusiast, we've got you covered. Simply search our extensive database spanning all platforms - PC, PlayStation, Xbox, and even indie gems on itch.io - and find the game that suits your style.
         </Banner.Text>
+      </Banner>
 
-        <Banner.Image
-        imageSrc={image01} 
-        altText="a collection of retro game `paraphernalia" 
-        />
+      <Banner imageSrc={image02} altText="an Xbox controller on a mousepad" imagePosition="left">
+        <Banner.Icon>
+          <SiGamebanana />
+        </Banner.Icon>
 
+        <Banner.Heading>
+          Never miss out on an epic adventure again!
+        </Banner.Heading>
+
+        <Banner.Text>
+          Save games you're interested in for later, and when you find those games that truly captivate you, add them to your favorites list for quick access whenever the gaming mood strikes.
+        </Banner.Text>
       </Banner>
       
-      {/* <div className="grid md:grid-cols-2 gap-2 relative">
-
-          <div className="p-4 flex items-center justify-center">
-
-            <div>
-              <h2 className="text-l text-center m-32">For cross-platform gamers</h2>
-              <p>Whether you're a PC aficionado, a console connoisseur, or an indie game enthusiast, we've got you covered. Simply search our extensive database spanning all platforms - PC, PlayStation, Xbox, and even indie gems on itch.io - and find the game that suits your style.</p>
-            </div>
-
-            <div className="icon-1 absolute top-8 right-8">
-              <IoGameControllerOutline 
-              className="text-6xl" 
-              role="img" aria-hidden="true"
-              />
-            </div>
-
-          </div>
-
-          <div className="image-blend-screen">
-            <Image className="fade-img" src={image01} alt="a collection of retro game `paraphernalia" />
-          </div>
-
-      </div> */}
-
-      <div className="grid md:grid-cols-2 gap-2">
-
-        <div className="p-4 flex items-center justify-center md:order-last relative">
-
-            <div>
-              <h2 className="text-xl text-center m-4">never miss out on an epic adventure again!</h2>
-              
-              <p>Save games you're interested in for later, and when you find those games that truly captivate you, add them to your favorites list for quick access whenever the gaming mood strikes.</p>
-            </div>
-
-            <div className="icon-1 absolute top-8 left-8">
-              <SiGamebanana 
-              className="text-6xl" 
-              role="img" aria-hidden="true"
-              />
-            </div>
-
-          </div>
-
-        <div className="image-blend-screen md:order-first">
-          <Image className="fade-img" src={image02} alt="an Xbox controller on a backlit mousepad" />
-        </div>
-        
-      </div>
+      {/* Last row */}
+      <div className="relative">
       
-      <div className="grid md:grid-cols-1 gap-2 relative">
-      
-        <div className="flex flex-col items-center justify-center m-32">
+        <div className="flex flex-col items-center justify-center gap-8 m-32">
 
-          <div className="icon-1 absolute top-8 right-8">
-            <MdGamepad
-            className="text-6xl" 
+          <div className="icon-1 absolute top-16 left-16">
+            <SiGamejolt
+            className="text-4xl" 
             role="img" aria-hidden="true"
             />
           </div>
 
-          <ul>
+          <ul className="text-lg text-center max-w-2xl">
             <li><strong>One-Stop Gaming Hub:</strong> Access thousands of games across multiple platforms in one centralized location.</li>
             <li><strong>Save for Later:</strong> Create your personalized wishlist and keep track of games you're eager to play.</li>
             <li><strong>Never Miss a Beat:</strong> Easily save your favorite games for quick access and endless entertainment.</li>
