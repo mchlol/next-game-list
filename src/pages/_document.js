8f75286a6d0c0255829a12e71d4fe19882157563
Michelle Brown <m.andreabr@gmail.com>
2024-03-17 18:00:07 +1100

    change daisy theme colours

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -2,7 +2,7 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
-    <Html lang="en" data-theme="synthwave">
+    <Html lang="en" data-theme="purple">
       <Head />
       <body>
         <Main />
--- tailwind.config.js
+++ tailwind.config.js
@@ -18,9 +18,25 @@ module.exports = {
   },
   plugins: [require("daisyui")],
   daisyui: {
-    themes: ['retro', 'synthwave'],
-    darkTheme: 'synthwave',
-    lightTheme: 'retro',
+    themes: [
+      {
+        purple: {
+          "primary": "#b646ba",
+          "primary-content": "#fff",
+          "secondary": "#551a89", 
+          "secondary-content": "#fff",
+          "accent": "#b888e2",
+          "neutral": "#ffffff",
+          "base-100": "#292524",
+          "base-content": "#fff",
+          "info": "#7dd3fc",
+          "success": "#bef264",
+          "warning": "#fcd34d",
+          "error": "#fda4af",
+          },
+      },
+      "night"
+    ],
     themeRoot: ':root',
   },
 }
