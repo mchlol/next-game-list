b717b6b4ee391cbc2a6740c0692d1a4f1be1bcc0
Michelle Brown <m.andreabr@gmail.com>
2024-03-18 14:52:52 +1100

    change bg colour to match nav

--- src/components/AppFooter.js
+++ src/components/AppFooter.js
@@ -1,6 +1,6 @@
 export default function AppFooter() {
     return (
-        <footer className="app-footer p-4 bg-primary text-secondary-content text-center">
+        <footer className="app-footer p-4 bg-base-200 text-center">
             <div>
                 All data provided by <a href="https://rawg.io" rel="noreferrer" target="_blank">RAWG.io</a>
             </div>
