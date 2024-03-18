87e1d2e10b3c03479eddae0a580364331b42339c
Michelle Brown <m.andreabr@gmail.com>
2024-03-18 16:48:04 +1100

    update search page styles

--- src/pages/search/index.js
+++ src/pages/search/index.js
@@ -13,29 +13,46 @@ export default function SearchGames() {
 
     router.push( {
       pathname: '/search/results',
-      query: {searchQuery, page: 1},
-
+      query: {
+        searchQuery, 
+        page: 1
+      },
     })
     
   }
 
   return (
-    <div className="search-form p-4 flex flex-col justify-center align-middle">
-            
-        <form className="join" id="searchForm" onSubmit={handleSubmit}>
-            <Input bordered 
-            id="search-input"
-            type="text" 
-            value={searchQuery}
-            placeholder="Search for a game title" 
-            autoComplete="off"
-            className="join-item" 
-            onChange={ev => setSearchQuery(ev.target.value)} required/> 
-            <Button type="submit" className="btn btn-secondary join-item">Search</Button>
+    <div className="search-form p-4 flex flex-col justify-center align-middle height-minus-nav">
+
+      <div className="flex flex-col gap-4">
+
+        <h2 className="text-xl md:text-2xl">Happy gaming!</h2>
+
+        <form className="bg-base-200 p-4 rounded-lg shadow join z-10" id="searchForm" onSubmit={handleSubmit}>
+          <Input bordered
+          id="search-input"
+          type="text"
+          value={searchQuery}
+          placeholder="Search for a game title"
+          autoComplete="off"
+          className="join-item md:w-96"
+          onChange={ev => setSearchQuery(ev.target.value)} required/>
+          <Button type="submit" className="btn btn-secondary join-item">Search</Button>
         </form>
 
-        <div className="p-4"></div>
-        <h3 className="text-xl">Happy gaming!</h3>
+        <small><em>Coming soon: search sort and filter!</em></small>
+
+      </div>
+
+      {/* filter where genre includes 'action' or platform includes 'playstation' */}
+      {/* sort by release date or metacritic score */}
+
+      {/* <ul className="p-4 max-w-[75ch]">
+        <li>One-Stop Gaming Hub: Access thousands of games across multiple platforms in one centralized location.</li>
+        <li>Save for Later: Create your personalized wishlist and keep track of games you're eager to play.</li>
+        <li>Never Miss a Beat: Easily save your favorite games for quick access and endless entertainment.</li>
+      </ul> */}
+
 
     </div>
   )
