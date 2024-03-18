1648453eb397f9eae98fbf421473c3e122ab8595
Michelle Brown <m.andreabr@gmail.com>
2024-03-18 16:49:37 +1100

    import font

--- src/components/Banner/BannerHeading.js
+++ src/components/Banner/BannerHeading.js
@@ -1,5 +1,7 @@
+import { silkscreen } from "@/fonts"
+
 export default function BannerHeading({children}) {
     return (
-        <h2 className="text-3xl text-center p-4 md:p-8">{children}</h2>
+        <h2 className={`text-3xl text-center p-4 md:p-8 ${silkscreen.className}`}>{children}</h2>
     )
 }
