ff137d8eed7db9f62b0f391882190d62cb113528
Michelle Brown <m.andreabr@gmail.com>
2024-03-18 15:27:16 +1100

    add call to action to landing page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,9 @@
-import { Button } from "react-daisyui";
+import { Button, Card } from "react-daisyui";
 import Hero from "@/components/Hero";
 import Image from "next/image";
 import image01 from '../assets/lorenzo-herrera-p0j-mE6mGo4-unsplash.jpg'
 import image02 from "../assets/jose-gil-2pNdTBn4C7U-unsplash.jpg"
-// import image03 from "../assets/nikita-kostrykin-JmUl_t_v3dw-unsplash.jpg"
+import image03 from "../assets/nikita-kostrykin-JmUl_t_v3dw-unsplash.jpg"
 import Link from "next/link";
 import { IoGameControllerOutline } from "react-icons/io5";
 import { SiGamebanana } from "react-icons/si";
@@ -18,59 +18,60 @@ export default function Home() {
 
       <Hero />
 
-      <Banner imageSrc={image01} altText="a collection of retro game paraphernalia" imagePosition="right">
-        <Banner.Icon>
-          <IoGameControllerOutline />
-        </Banner.Icon>
-
-        <Banner.Heading>
-          For cross-platform gamers
-        </Banner.Heading>
-
-        <Banner.Text>
-          Whether you're a PC aficionado, a console connoisseur, or an indie game enthusiast, we've got you covered. Simply search our extensive database spanning all platforms - PC, PlayStation, Xbox, and even indie gems on itch.io - and find the game that suits your style.
-        </Banner.Text>
-      </Banner>
-
-      <Banner imageSrc={image02} altText="an Xbox controller on a mousepad" imagePosition="left">
-        <Banner.Icon>
-          <SiGamebanana />
-        </Banner.Icon>
-
-        <Banner.Heading>
-          Never miss out on an epic adventure again!
-        </Banner.Heading>
-
-        <Banner.Text>
-          Save games you're interested in for later, and when you find those games that truly captivate you, add them to your favorites list for quick access whenever the gaming mood strikes.
-        </Banner.Text>
-      </Banner>
+        <Banner imageSrc={image01} altText="a collection of retro game paraphernalia" imagePosition="right">
+          <Banner.Icon>
+            <IoGameControllerOutline />
+          </Banner.Icon>
+          <Banner.Heading>
+            For cross-platform gamers
+          </Banner.Heading>
+          <Banner.Text>
+            Whether you're a PC aficionado, a console connoisseur, or an indie game enthusiast, we've got you covered. Simply search our extensive database spanning all platforms - PC, PlayStation, Xbox, and even indie gems on itch.io - and find the game that suits your style.
+          </Banner.Text>
+        </Banner>
+        <Banner imageSrc={image02} altText="an Xbox controller on a mousepad" imagePosition="left">
+          <Banner.Icon>
+            <SiGamebanana />
+          </Banner.Icon>
+          <Banner.Heading>
+            Never miss out on an epic adventure again!
+          </Banner.Heading>
+          <Banner.Text>
+            Save games you're interested in for later, and when you find those games that truly captivate you, add them to your favorites list for quick access whenever the gaming mood strikes.
+          </Banner.Text>
+        </Banner>
       
       {/* Last row */}
       <div className="relative">
       
         <div className="flex flex-col items-center justify-center gap-8 m-32">
 
-          <div className="icon-1 absolute top-16 left-16">
+          <div className="icon-1 absolute top-8">
             <SiGamejolt
             className="text-4xl" 
             role="img" aria-hidden="true"
             />
           </div>
 
-          <ul className="text-lg text-center max-w-2xl">
-            <li><strong>One-Stop Gaming Hub:</strong> Access thousands of games across multiple platforms in one centralized location.</li>
-            <li><strong>Save for Later:</strong> Create your personalized wishlist and keep track of games you're eager to play.</li>
-            <li><strong>Never Miss a Beat:</strong> Easily save your favorite games for quick access and endless entertainment.</li>
-          </ul>
+          <Card imageFull bordered={false} className="text-primary md:max-w-md">
+            <Card.Image src={image03.src} alt="image"/>
 
-          <h2 className="text-xl text-center">Ready to explore?</h2>
+            <Card.Body className="flex flex-col justify-center items-center bg-secondary/50">
+              <Card.Title tag="h2" className="md:text-2xl text-center text-neutral" style={{textShadow: '2px 2px 5px #b646ba'}}>Ready to explore?</Card.Title>
+            
+              <Card.Actions className="justify-center">
+                <Button className="mt-4">
+                  <Link href="/search">
+                    Search now!
+                  </Link>
+                </Button>
+              </Card.Actions>
 
-          <Button className="mt-4">
-            <Link href="/searchgames">
-              Search now!
-            </Link>
-          </Button>
+            </Card.Body>
+
+          </Card>
+
+          
 
         </div>
         
