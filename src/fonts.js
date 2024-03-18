e6c78b37dc5ea68ffcd1355a50f56a70c63d0f83
Michelle Brown <m.andreabr@gmail.com>
2024-03-18 14:33:34 +1100

    import fonts

---
+++ src/fonts.js
@@ -0,0 +1,12 @@
+import { Silkscreen, Montserrat } from 'next/font/google'
+
+export const silkscreen = Silkscreen({
+    subsets: ['latin'],
+    display: 'swap',
+    weight: '400'
+});
+
+export const montserrat = Montserrat({
+    subsets: ['latin'],
+    display: 'swap'
+})
