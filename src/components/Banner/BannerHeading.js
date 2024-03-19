505bfe13e9452c64843a50bc298d7645124096b9
Michelle Brown <m.andreabr@gmail.com>
2024-03-19 11:35:59 +1100

    add pink text shadow to span

--- src/components/Banner/BannerHeading.js
+++ src/components/Banner/BannerHeading.js
@@ -2,6 +2,6 @@ import { silkscreen } from "@/fonts"
 
 export default function BannerHeading({children}) {
     return (
-        <h2 className={`text-3xl text-center p-4 md:p-8 ${silkscreen.className}`}>{children}</h2>
+        <h2 className={`text-3xl text-center p-4 md:p-8 text-shadow-pink ${silkscreen.className}`}>{children}</h2>
     )
 }
