d02b6e21427f8b531afcd0551f40a54ec0d5bce8
Michelle Brown <m.andreabr@gmail.com>
2024-03-19 11:37:24 +1100

    add pink text shadow to span

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -6,9 +6,9 @@ export default function Hero() {
     return (
         <div className="home-hero p-2 flex flex-col items-center justify-center gap-8 w-full relative height-minus-nav">
 
-            <h1 className="lg:text-9xl text-6xl break-all text-center">GameList</h1>
+            <h1 className="lg:text-9xl text-6xl break-all text-center text-shadow-pink">GameList</h1>
 
-            <h2 className="md:text-l text-center">Create the endless backlog of your dreams!</h2>
+            <h2 className="md:text-l text-center">Create the <span className="text-shadow-pink">endless backlog</span> of your dreams!</h2>
 
             <Button className="mt-4">
             <Link href="/search">
