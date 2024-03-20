c0cca7165006a73554eb01206b87f6d1eb01d5b0
Michelle Brown <m.andreabr@gmail.com>
2024-03-20 17:27:56 +1100

    update classes for buttons to switch views

--- src/pages/lists.js
+++ src/pages/lists.js
@@ -1,18 +1,37 @@
+import { useEffect, useState } from 'react';
 import ListDisplay from '@/components/ListDisplay';
-import { Divider } from 'react-daisyui';
-import { FaHeart } from 'react-icons/fa6';
+import { Button } from 'react-daisyui';
+
 export default function Lists() {
+
+    const [activeTab, setActiveTab] = useState('wishlist');
 
     return (
         <div className="lists-wrap flex flex-col min-h-screen">
 
-            <ListDisplay listName="wishlist" />
+            <div className="flex flex-wrap justify-center items-center gap-2 m-4 mt-8">
+                <p>Select view:</p>
+                <Button className={`btn btn-sm ${activeTab === 'wishlist' ? 'btn-success' : 'opacity-50'}`}
+                onClick={() => {
+                    setActiveTab(() => 'wishlist')
+                    }
+                }
+                >
+                    Wishlist
+                </Button>
+                <Button className={`btn btn-sm ${activeTab === 'favourites' ? 'btn-success' : 'opacity-50'}`}
+                onClick={() => {
+                    setActiveTab(() => 'favourites')
+                    }
+                }
+                    >
+                    Favourites
+                </Button>
+            </div>
 
-            <Divider color="primary">
-                <FaHeart className="text-3xl" />
-            </Divider>
-
-            <ListDisplay listName="favourites" />
+            { 
+                <ListDisplay listName={activeTab} />
+            }
             
         </div>
     )
