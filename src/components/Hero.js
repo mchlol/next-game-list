8942259f25a154b053c6396b473163a782a7cfb3
Michelle Brown <m.andreabr@gmail.com>
2024-03-21 08:25:43 +1100

    change button text

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -12,7 +12,7 @@ export default function Hero() {
 
             <Button className="mt-4">
             <Link href="/search">
-                Search now!
+                Go to search
             </Link>
             </Button>
 
