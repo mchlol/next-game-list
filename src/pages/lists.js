332e01e82b34bdea897da845f91f61ba97aa9138
Michelle Brown <m.andreabr@gmail.com>
2024-03-20 15:03:04 +1100

    set list container to flex and add a divider between categories

--- src/pages/lists.js
+++ src/pages/lists.js
@@ -1,10 +1,16 @@
 import ListDisplay from '@/components/ListDisplay';
+import { Divider } from 'react-daisyui';
+import { FaHeart } from 'react-icons/fa6';
 export default function Lists() {
 
     return (
-        <div className="lists-wrap">
+        <div className="lists-wrap flex flex-col min-h-screen">
 
             <ListDisplay listName="wishlist" />
+
+            <Divider color="primary">
+                <FaHeart className="text-3xl" />
+            </Divider>
 
             <ListDisplay listName="favourites" />
             
