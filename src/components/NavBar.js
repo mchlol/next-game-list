0ec1a6cdaa2de0f5a73abc0863e3b09b2c68f02e
Michelle Brown <m.andreabr@gmail.com>
2024-03-21 15:30:16 +1100

    set max width for navbar so it doesnt overflow page content on large screens

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,7 +5,7 @@ import { silkscreen } from "@/fonts";
 export default function NavBar() {
     
     return (
-        <Navbar className={`fixed z-10 p-4 flex justify-between gap-4 bg-secondary ${silkscreen.className}`}>
+        <Navbar className={`fixed z-10 p-4 flex justify-between gap-4 bg-secondary ${silkscreen.className} max-w-[1440px]`}>
 
             <div className="flex-none pl-4">
                 <span className="md:text-xl text-shadow-pink">
