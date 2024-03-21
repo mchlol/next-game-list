2cbdc6fe6919b7173e71d579033de9494d8375c5
Michelle Brown <m.andreabr@gmail.com>
2024-03-21 16:25:33 +1100

    add backdrop filter to blur background under navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,7 +5,8 @@ import { silkscreen } from "@/fonts";
 export default function NavBar() {
     
     return (
-        <Navbar className={`fixed z-10 p-4 flex justify-between gap-4 bg-secondary ${silkscreen.className} max-w-[1440px]`}>
+        <Navbar className={`fixed z-10 p-4 flex justify-between gap-4 ${silkscreen.className} max-w-[1440px]`}
+        style={{backdropFilter: "blur(5px)"}}>
 
             <div className="flex-none pl-4">
                 <span className="md:text-xl text-shadow-pink">
