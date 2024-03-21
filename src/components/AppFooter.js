92617826bc17addba9f43759bacc8116a0994523
Michelle Brown <m.andreabr@gmail.com>
2024-03-21 17:00:14 +1100

    use classes to set height and width of footer

--- src/components/AppFooter.js
+++ src/components/AppFooter.js
@@ -1,6 +1,6 @@
 export default function AppFooter() {
     return (
-        <footer className="app-footer p-4 bg-base-200 text-center">
+        <footer className="app-footer p-4 bg-base-200 text-center h-16 w-full">
             <div>
                 All data provided by <a href="https://rawg.io" rel="noreferrer" target="_blank">RAWG.io</a>
             </div>
