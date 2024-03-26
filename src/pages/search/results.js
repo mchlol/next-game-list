90e52b1863997c961fd5d0867a1b7584b658e493
Michelle Brown <m.andreabr@gmail.com>
2024-03-26 15:56:32 +1100

    get params string and use in fetch

--- src/pages/search/results.js
+++ src/pages/search/results.js
@@ -5,18 +5,30 @@ import GameCard from "@/components/GameCard";
 import { Button } from "react-daisyui";
 import PaginateSearch from "@/components/PaginateSearch";
 import { FaArrowLeft } from "react-icons/fa6";
+import { getParamsString } from "@/functions";
 
-function Search( {data, title, page, totalPages} ) {
+function Search( {data, title, page, totalPages, query, paramsString} ) {
 
     const router = useRouter();
+
+    console.log('query: ', query);
+    console.log('paramsString: ',paramsString)
     
     const [games, setGames] = useState(data.results);
-    const [loading, setLoading] = useState(false);
     const currentPage = parseInt(page) || 1;
+
+    const currentParams = {
+        ...query,
+        page: currentPage
+    }
+
+    console.log(currentParams);
+    const currentParamsString = getParamsString(currentParams);
+    console.log(currentParamsString);
 
     useEffect( () => {
         const fetchData = async () => {
-            const newData = await handleFetch(`https://rawg.io/api/games?search=${title}&page=${currentPage}&page_size=24&search_precise=true&token&key=${process.env.NEXT_PUBLIC_API_KEY}`);
+            const newData = await handleFetch(`https://rawg.io/api/games${currentParamsString}&page_size=24&search_precise=true&token&key=${process.env.NEXT_PUBLIC_API_KEY}`);
             setGames(newData.results);
         };
         fetchData();
@@ -27,7 +39,6 @@ function Search( {data, title, page, totalPages} ) {
     return (
         <>
             {
-               
                 games.length > 0
                 ?
                 <div className="p-4">
@@ -67,7 +78,6 @@ function Search( {data, title, page, totalPages} ) {
 
                 </div>
 
-
             }
         </>
     )
@@ -88,8 +98,17 @@ export async function getServerSideProps(context) {
 
     const BASE_URL = 'https://rawg.io/api';
     const API_KEY = process.env.NEXT_PUBLIC_API_KEY;
-    
-    const data = await handleFetch(`${BASE_URL}/games?search=${title}&page=${page}&page_size=${perPage}&search_precise=true&token&key=${API_KEY}`);
+
+    // the URL will change based on if certain params are present
+    // form a string out of all available params and insert it into the URL
+    // page is always 1 
+    // so if we have title: 'prey' string is `?search=prey&page=1`
+    // get this from context.query so const PARAMS = `?search=${query.title}&page=${query.page}`
+    // then URL will be `${BASE_URL}/games${PARAMS}&page_size=${perPage}&search_precise=true&token&key=${API_KEY}`
+
+    const paramsString = getParamsString(query);
+
+    const data = await handleFetch(`${BASE_URL}/games${paramsString}&page_size=${perPage}&search_precise=true&token&key=${API_KEY}`);
 
     const totalResults = data.count;
     const totalPages = Math.ceil(totalResults / perPage);
@@ -99,7 +118,9 @@ export async function getServerSideProps(context) {
             data,
             title,
             page,
-            totalPages
+            totalPages, // test
+            query,
+            paramsString
         }
     }
 }
