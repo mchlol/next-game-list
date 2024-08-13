992a1d223fce1cfc384b3fd1587064216aa58f64
Michelle Brown <m.andreabr@gmail.com>
2024-08-13 12:34:10 +1000

    create and import error boundary component

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,16 +1,17 @@
 import '@/styles/globals.css'
 import Head from 'next/head'
 import Layout from './layout'
+import ErrorBoundary from '@/components/ErrorBoundary'
 
 export default function App({ Component, pageProps }) {
   return (
-    <>
+    <ErrorBoundary>
     <Head>
       <title>GameList</title>
     </Head>
     <Layout>
       <Component {...pageProps} />
     </Layout>
-  </>
+  </ErrorBoundary>
   )
 }
